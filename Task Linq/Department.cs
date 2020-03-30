1c34cdbd8063a7c4e5087da81c8e23bccc77b873
Shobhit Walia <shobhitwalia@gmail.com>
2020-03-30 17:09:30 +0530

    Add files via upload

---
+++ Task Linq/Department.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assign.Task_Linq
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+    }
+}
