b670e65238a0e60273e15b38561720c57881cf30
Shobhit Walia <shobhitwalia@gmail.com>
2020-03-30 17:08:20 +0530

    Add files via upload
    Initial commit

---
+++ Assign/Assign/Exception Handling/SampleException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assign.Exception_Handling
+{
+    class SampleException
+    {
+
+        class MyCustomException : Exception
+        {
+            public MyCustomException(string name) : base(String.Format("{0}", name))
+            {
+
+            }
+        }
+        public void Exception()
+        {
+            string[] list = new string[5];
+            list[0] = "Sunday";
+            list[1] = "Monday";
+            list[2] = "Tuesday";
+            list[3] = "Wednesday";
+            list[4] = "Thursday";
+
+            for (int i = 0; i <= 5; i++)
+            {
+                if (i == 5) throw new MyCustomException("Out of range");
+                Console.WriteLine(list[i].ToString());
+
+                // Output System.IndexOutOfRangeException
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+}
---
+++ Assign/Assign/Memory Managment/MyClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assign.MemoryManagment
+{
+    class MyClass : IDisposable
+    {
+        public int myInteger = 88;
+        public void Dispose()
+        {
+            Console.WriteLine("Disposing");
+        }
+    }
+}
