2957ffeeabac2d9212fe2fbe2dee7e955ddf38f7
Shobhit Walia <shobhitwalia@gmail.com>
2020-04-07 20:02:43 +0530

    Add files via upload

---
+++ Assign/Assign/Expando/Expando.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assign.Expando
+{
+   public static class ExpandoCustomClass
+    {
+        public static void PrintEmployeeDetails(dynamic person)
+        {
+            Console.WriteLine("{0} has {1} its email is {2}.", person.Name, person.Email);
+            Console.ReadLine();
+        }
+    }
+}
